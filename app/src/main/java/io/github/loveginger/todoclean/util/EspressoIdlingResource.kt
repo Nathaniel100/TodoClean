c5b657a11540b6c9f59aed62eae4040c58d84467
吴凡 <>
2018-09-03 18:03:02 +0800

    Fix a IdlingResource error.

--- app/src/main/java/io/github/loveginger/todoclean/util/EspressoIdlingResource.kt
+++ app/src/main/java/io/github/loveginger/todoclean/util/EspressoIdlingResource.kt
@@ -1,5 +1,8 @@
 package io.github.loveginger.todoclean.util
 
+import android.support.test.espresso.IdlingResource
+import java.util.concurrent.atomic.AtomicInteger
+
 object EspressoIdlingResource {
 
   private val resource = "GLOBAL"
@@ -8,6 +11,39 @@ object EspressoIdlingResource {
 
   fun increment() = idlingResource.increment()
 
-  fun decrement() = idlingResource.increment()
+  fun decrement() = idlingResource.decrement()
+}
+
+class SimpleCountingIdlingResource(private val resourceName: String) : IdlingResource {
+  private val counter = AtomicInteger(0)
+
+  private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+  override fun getName(): String {
+    return resourceName;
+  }
+
+  override fun isIdleNow(): Boolean {
+    return counter.get() == 0
+  }
+
+  override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback?) {
+    this.resourceCallback = resourceCallback
+  }
+
+  fun increment() {
+    counter.getAndIncrement()
+  }
+
+  fun decrement() {
+    val counterVal = counter.decrementAndGet()
+    if (counterVal == 0) {
+      resourceCallback?.onTransitionToIdle()
+    }
+
+    if (counterVal < 0) {
+      throw IllegalStateException("Counter has been corrupted")
+    }
+  }
 
 }
